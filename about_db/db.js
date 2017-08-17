984c14de784f6f64b156acd406e4399f88cbefec
dh527200 <401193007@qq.com>
2017-08-17 21:03:38 +0800

    about mongoose event

---
+++ about_db/db.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var DB_URL = "mongodb://localhost:27017/test";
+
+mongoose.connect(DB_URL);  //连接数据库
+
+mongoose.connection.on("connected",function(){
+	console.log('Mongoose connection open to ' + DB_URL);  
+});
+
+mongoose.connection.on("error",function(err){
+	console.log(err);
+});
+
+mongoose.connection.on("disconnected",function(){
+	console.log("连接断开");
+});
